b9a5d79c46892663f8be9e149c1616b8d3a3c1ff
DESKTOP-GTFEJ75\Ryzen <mogamiyuki9117@gmail.com>
2024-01-07 14:30:24 +0900

    初版

---
+++ TouchUi.js
@@ -0,0 +1,46 @@
+//=============================================================================
+// TouchUi.js
+// ----------------------------------------------------------------------------
+// (C)2024 最上ユキ
+// This plugin is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0 2024/01/07 初版
+// ----------------------------------------------------------------------------
+// [Twitter]: https://twitter.com/mogamiyuki_noc
+//=============================================================================
+
+/*:ja
+ * @target MZ
+ * @url
+ * @plugindesc タッチUIのON/OFFに対応して上部の余白を削除するプラグイン
+ * @author 最上ユキ
+ *
+ * @help タッチUIのON/OFFに対応して上部の余白を削除します。
+ *
+ * オプションの「タッチUI」設定値によって、以下の制御を行います。
+ * ・ON の場合：余白ありに変更
+ * ・OFFの場合：余白なしに変更
+ *
+ * このプラグインには、プラグインコマンドはありません。
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、R18利用等）
+ *  についても制限はありません。
+ *  このプラグインはもうあなたのものです。
+ */
+
+(() => {
+    'use strict';
+    const pluginName = "TouchUi";
+
+    const ButtonAreaHeightSwitch = Scene_Base.prototype.buttonAreaHeight;
+    Scene_Base.prototype.buttonAreaHeight = function() {
+      if (ConfigManager.touchUI) {
+        return 52;
+      } else {
+        return 0;
+      }
+    };
+})();
